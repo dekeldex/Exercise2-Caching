341dcf5d7d7274078d02d1e02a9c6742a035ccfe
Dekel Binyamin <41626906+dekeldex@users.noreply.github.com>
2021-06-29 20:48:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import uuid
 import json  
-from datetime import datetime
 from flask import Flask, request
 
-
-r = redis.Redis(host='localhost', port=6379, db=0)
+memory = {}
 
 app = Flask(__name__)
 
@@ -12,43 +10,42 @@ app = Flask(__name__)
 def healthCheck():
     return true
 
-@app.route('/entry')
-def enter():
-    plate = request.args.get('plate')
-    parkingLot = request.args.get('parkingLot')
-    id = str(uuid.uuid4())
-    ticket = {"id": id, 
-              "plate": plate, 
-              "parkingLot": parkingLot, 
-              "entry": datetime.now().isoformat() }
-    r.set(id, json.dumps(ticket))
-    return id, 201
+@app.route('/put')
+def put():
+    str_key = request.args.get('str_key')
+    data = request.args.get('data')
+    expiration_date = request.args.get('expiration_date')
+    memory["str_key"] = {
+        "data": data,
+        "expiration_date" = expiration_date
+    }
+    return "YAY", 201
 
 
-@app.route('/exit')
-def exit():
-    id = request.args.get("ticketId")
-    ticket_str = r.get(id)
-    if ticket_str is None:
-        return "No such ticket", 404
+# @app.route('/exit')
+# def exit():
+#     id = request.args.get("ticketId")
+#     ticket_str = r.get(id)
+#     if ticket_str is None:
+#         return "No such ticket", 404
 
-    ticket = json.loads(ticket_str)
+#     ticket = json.loads(ticket_str)
 
-    if "exit" in ticket:
-        return ticket_str, 406
+#     if "exit" in ticket:
+#         return ticket_str, 406
 
-    entry_time = datetime.fromisoformat(ticket["entry"])
-    exit = datetime.now()
-    diff_in_minutes = int((exit - entry_time).total_seconds() / 60)
-    charges = diff_in_minutes / 15
-    if diff_in_minutes % 15 != 0:
-        charges += 1
+#     entry_time = datetime.fromisoformat(ticket["entry"])
+#     exit = datetime.now()
+#     diff_in_minutes = int((exit - entry_time).total_seconds() / 60)
+#     charges = diff_in_minutes / 15
+#     if diff_in_minutes % 15 != 0:
+#         charges += 1
     
-    ticket["exit"] = exit.isoformat()
-    ticket["chargs"] = charges
-    ticket["total_cost"] = charges * 2.5
+#     ticket["exit"] = exit.isoformat()
+#     ticket["chargs"] = charges
+#     ticket["total_cost"] = charges * 2.5
 
-    r.set(id, json.dumps(ticket))
+#     r.set(id, json.dumps(ticket))
 
-    return ticket
+#     return ticket
 
